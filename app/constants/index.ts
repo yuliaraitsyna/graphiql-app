6551437e1c59cfcb2a4fe824d87724216f6f8221
Elizaveta <lizatukailo@gmail.com>
2024-08-28 00:07:33 +0300

    feat: add details about the team

---
+++ app/constants/index.ts
@@ -0,0 +1,26 @@
+export const team = [
+  {
+    image: '/app/assets/images/Evgenii-Artemenko.jpg',
+    git: 'https://github.com/playoffthecuff',
+    name: 'Evgenii Artemenko',
+    role: 'Team Lead, Frontend Developer',
+    description:
+      'Energy specialist with a background in Management and Informatics in Technical Systems, focusing on automating technological processes. Participated in the development of a product line for high voltage power distribution. I like to develop interfaces.',
+  },
+  {
+    image: '/app/assets/images/Elizaveta-Tukailo.jpeg',
+    git: 'https://github.com/elizaveta-tukailo',
+    name: 'Elizaveta Tukailo',
+    role: 'Frontend Developer',
+    description:
+      'Web developer with a focus on goal-achievement strategies. My skills encompass a wide range of topics and tools, enabling me to work effectively and efficiently on various projects. I am dedicated to continuous learning and adapting to new technologies.',
+  },
+  {
+    image: '/app/assets/images/Yulia-Raitsyna.jpeg',
+    git: 'https://github.com/yuliaraitsyna',
+    name: 'Yulia Raitsyna',
+    role: 'Frontend Developer',
+    description:
+      'Frontend developer with applied skills in computer science and mathematics. Skillful in web and software development. Experienced in modern development tools and frameworks.',
+  },
+];
