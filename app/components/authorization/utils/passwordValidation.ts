112840549f5385b7fac48d8cd99aa1ba441773b4
yuliaraitsyna <yuliaraitsyna@gmail.com>
2024-08-28 12:24:16 +0300

    chore: add unicode support

--- app/components/authorization/utils/passwordValidation.ts
+++ app/components/authorization/utils/passwordValidation.ts
@@ -3,20 +3,20 @@ export const handlePasswordCheck = (password: string): string | null => {
     return 'Password must be at least 8 characters long.';
   }
 
-  const hasUpperCase = /[A-Z]/.test(password);
+  const hasUpperCase = /[\p{Lu}]/u.test(password);
   const hasNumber = /[0-9]/.test(password);
-  const hasSpecialChar = /[!@#$%^&*]/.test(password);
+  const hasSpecialChar = /[\p{S}\p{P}]/u.test(password);
 
   if (!hasUpperCase) {
-    return 'Password must contain at least one uppercase letter.';
+    return 'Password must contain at least one uppercase letter (unicode is spported).';
   }
 
   if (!hasNumber) {
-    return 'Password must contain at least one number.';
+    return 'Password must contain at least one number (unicode is spported).';
   }
 
   if (!hasSpecialChar) {
-    return 'Password must contain at least one special character';
+    return 'Password must contain at least one special character (unicode is spported)';
   }
 
   return null;
