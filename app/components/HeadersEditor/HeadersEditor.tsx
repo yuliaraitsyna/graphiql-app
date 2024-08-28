565b6fd1ca2c42142fd16a9a97fbc4a4d7b7a6f8
yuliaraitsyna <yuliaraitsyna@gmail.com>
2024-08-28 19:45:21 +0300

    chore: add editing and deleting

--- app/components/HeadersEditor/HeadersEditor.tsx
+++ app/components/HeadersEditor/HeadersEditor.tsx
@@ -18,20 +18,52 @@ import {Header} from './models/header';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 
+const initialHeader: Header = {
+  key: '',
+  value: '',
+  checked: false,
+};
+
 const HeadersEditor: React.FC = () => {
   const [headers, setHeaders] = useState<Header[]>([]);
+  const [editingIndex, setEditingIndex] = useState<number | null>(null);
 
   const handleHeaderAddition = () => {
-    const header: Header = {
-      key: '',
-      value: '',
-      checked: false,
-    };
-    setHeaders([...headers, header]);
+    setHeaders([...headers, initialHeader]);
+  };
+
+  const handleEditClick = (index: number) => {
+    setEditingIndex(index);
+  };
+
+  const handleHeaderChange = (field: keyof Header, value: string) => {
+    if (editingIndex !== null) {
+      const updatedHeaders = [...headers];
+      updatedHeaders[editingIndex] = {...updatedHeaders[editingIndex], [field]: value};
+      setHeaders(updatedHeaders);
+    }
+  };
+
+  const handleCheckboxChange = (index: number) => {
+    const updatedHeaders = [...headers];
+    updatedHeaders[index].checked = !updatedHeaders[index].checked;
+    setHeaders(updatedHeaders);
+  };
+
+  const handleEditFinish = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      event.currentTarget.blur();
+      setEditingIndex(null);
+    }
+  };
+
+  const handleDelete = (index: number) => {
+    const updatedHeaders = headers.filter((_, i) => i !== index);
+    setHeaders(updatedHeaders);
   };
 
   return (
-    <Container sx={{width: '90%', border: '1px solid red', padding: 2}}>
+    <Container sx={{width: '90%', padding: 2}}>
       <Box sx={{width: '100%'}} display={'flex'} flexDirection={'row'} justifyContent={'space-between'} mb={2}>
         <Typography component={'h6'} variant="h6">
           Headers
@@ -58,22 +90,37 @@ const HeadersEditor: React.FC = () => {
             {headers.map((header, index) => (
               <TableRow key={index}>
                 <TableCell sx={{border: '1px solid', borderColor: grey[200]}}>
-                  <Checkbox checked={header.checked}></Checkbox>
+                  <Checkbox checked={header.checked} onChange={() => handleCheckboxChange(index)}></Checkbox>
+                </TableCell>
+                <TableCell sx={{border: '1px solid', borderColor: grey[200]}}>
+                  <Input
+                    value={header.key}
+                    disableUnderline
+                    fullWidth
+                    disabled={index !== editingIndex}
+                    onChange={e => handleHeaderChange('key', e.target.value)}
+                    onKeyDown={handleEditFinish}
+                  />
+                </TableCell>
+                <TableCell sx={{border: '1px solid', borderColor: grey[200]}}>
+                  <Input
+                    value={header.value}
+                    disableUnderline
+                    fullWidth
+                    disabled={index !== editingIndex}
+                    onChange={e => handleHeaderChange('value', e.target.value)}
+                    onKeyDown={handleEditFinish}
+                  />
                 </TableCell>
                 <TableCell sx={{border: '1px solid', borderColor: grey[200]}}>
                   <Box display={'flex'} alignItems={'center'}>
-                    <Input value={header.value} />
-                    <Button>{<EditIcon />}</Button>
+                    <Button onClick={() => handleEditClick(index)}>
+                      <EditIcon />
+                    </Button>
+                    <Button onClick={() => handleDelete(index)}>
+                      <DeleteIcon />
+                    </Button>
                   </Box>
-                </TableCell>
-                <TableCell sx={{border: '1px solid', borderColor: grey[200]}}>
-                  <Box display={'flex'} alignItems={'center'}>
-                    <Input value={header.value} />
-                    <Button>{<EditIcon />}</Button>
-                  </Box>
-                </TableCell>
-                <TableCell sx={{border: '1px solid', borderColor: grey[200]}}>
-                  <Button>{<DeleteIcon />}</Button>
                 </TableCell>
               </TableRow>
             ))}
