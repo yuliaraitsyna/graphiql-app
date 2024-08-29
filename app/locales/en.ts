a6ee403a854765b04fdba1dd18a433d04414bf53
Elizaveta <lizatukailo@gmail.com>
2024-08-29 20:27:58 +0300

    feat: add translations

--- app/locales/en.ts
+++ app/locales/en.ts
@@ -1,8 +1,60 @@
 export default {
-  title: 'Title in English',
-  description: 'Hello to the team and mentors',
-  signIn: 'Sign in',
-  signUp: 'Sign up',
-  signOut: 'Sign out',
-  mainPage: 'Main page',
+  common: {
+    altAppLogo: 'App logo',
+  },
+  page: {
+    main: {
+      welcome: 'Welcome',
+      welcomeBack: 'Welcome back',
+      aboutProjectTitle: 'About project',
+      aboutUsTitle: 'About us',
+      aboutRSSchool:
+        'RS School is a free and community-based online education program conducted by The Rolling Scopes Community since 2013. Currently 500+ developers from different countries and companies involve in the education process as mentors.',
+      aboutCourse:
+        'React course provides by RS School is designed for developers who want to harness the power of one of the most popular JavaScript libraries in the world. Dive deep into the essentials of React and learn how to create dynamic, high-performance user interfaces. Throughout the course, you will engage in hands-on projects that solidify your understanding and give you practical experience. Learn best practices for building scalable applications, manage application state effectively, and optimize performance.',
+      restTitle: 'REST client',
+      restDescription:
+        'Traditional approaches to calling a REST API require the API user to know a programming language, understand the APIs protocol, and interpret the response. A REST client streamlines this process, enabling developers of all levels to explore, test, and debug REST APIs from an intuitive user interface.',
+      graphTitle: 'GraphiQL Client',
+      graphDescription:
+        'GraphQL is a query language for APIs and a runtime for fulfilling those queries with your existing data. GraphQL provides a complete and understandable description of the data in your API, gives clients the power to ask for exactly what they need and nothing more.',
+      aboutTeam:
+        'Now lets provide a short overview of the app that our team created as the final project for the React Course at RS School. There are two main features of our project:',
+      team: [
+        {
+          image: '/app/assets/images/Evgenii-Artemenko.jpg',
+          git: 'https://github.com/playoffthecuff',
+          name: 'Evgenii Artemenko',
+          role: 'Team Lead, Frontend Developer',
+          description:
+            'Energy specialist with a background in Management and Informatics in Technical Systems, focusing on automating technological processes. Participated in the development of a product line for high voltage power distribution. I like to develop interfaces.',
+        },
+        {
+          image: '/app/assets/images/Elizaveta-Tukailo.jpeg',
+          git: 'https://github.com/elizaveta-tukailo',
+          name: 'Elizaveta Tukailo',
+          role: 'Frontend Developer',
+          description:
+            'Web developer with a focus on goal-achievement strategies. My skills encompass a wide range of topics and tools, enabling me to work effectively and efficiently on various projects. I am dedicated to continuous learning and adapting to new technologies.',
+        },
+        {
+          image: '/app/assets/images/Yulia-Raitsyna.jpeg',
+          git: 'https://github.com/yuliaraitsyna',
+          name: 'Yulia Raitsyna',
+          role: 'Frontend Developer',
+          description:
+            'Frontend developer with applied skills in computer science and mathematics. Skillful in web and software development. Experienced in modern development tools and frameworks.',
+        },
+      ],
+    },
+  },
+  links: {
+    signIn: 'Sign in',
+    signOut: 'Sign out',
+    signUp: 'Sign up',
+    mainPage: 'Main page',
+    history: 'History',
+    restClient: 'Rest Client',
+    graphqlClient: 'GraphiQL Client',
+  },
 };
