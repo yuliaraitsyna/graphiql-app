d3460563cab09456e756c03447a3e0965ed83488
playoffthecuff <jmobile@bk.ru>
2024-09-02 22:34:26 +0300

    fix: improve code styling

--- app/components/JsonEditor/JsonEditor.tsx
+++ app/components/JsonEditor/JsonEditor.tsx
@@ -1,23 +1,23 @@
 import '@fontsource/roboto-mono';
-import css from './JsonEditor.module.css';
+import styles from './JsonEditor.module.css';
 import {useState} from 'react';
 import {Box, Button, Typography} from '@mui/material';
 import {EditIcon, VisibleIcon} from '../Icons';
 import prettifyJson from '~/utils/prettifyJson';
 
-type Props = {
-  mode?: 'view' | 'edit';
-  type?: 'JSON' | 'text';
-  defaultValue?: string;
-  onChange?: (v: string) => void;
+type EditorProps = {
+  mode: 'view' | 'edit';
+  type: 'JSON' | 'text';
+  defaultValue: string;
+  onChange: (v: string) => void;
 };
 
 function StrNum({content}: {content: string}) {
   const numStr = Array.from({length: content.split('\n').length}, (_, k) => k + 1).join('\n');
-  return <pre className={css.numeration}>{numStr}</pre>;
+  return <pre className={styles.numeration}>{numStr}</pre>;
 }
 
-export default function JsonEditor({mode = 'view', type = 'JSON', defaultValue = '', onChange}: Props) {
+export default function JsonEditor({mode = 'view', type = 'JSON', defaultValue = '', onChange}: Partial<EditorProps>) {
   const [content, setContent] = useState(type === 'JSON' ? prettifyJson(defaultValue) : defaultValue);
 
   const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
@@ -37,15 +37,15 @@ export default function JsonEditor({mode = 'view', type = 'JSON', defaultValue =
   };
 
   return (
-    <Box component="section" className={css.editor}>
-      <Box component="div" className={css.marker}>
+    <Box component="section" className={styles.editor}>
+      <Box component="div" className={styles.marker}>
         {mode === 'view' ? (
           <VisibleIcon style={{width: '12px', height: '12px'}} />
         ) : (
           <EditIcon style={{width: '12px', height: '12px'}} />
         )}
       </Box>
-      <Box component="div" className={css.heading}>
+      <Box component="div" className={styles.heading}>
         <Typography variant="subtitle1" component="p">
           {type === 'JSON' ? 'JSON ' : 'Text '} Content
         </Typography>
@@ -55,10 +55,10 @@ export default function JsonEditor({mode = 'view', type = 'JSON', defaultValue =
           </Button>
         )}
       </Box>
-      <Box component="div" className={css.wrapper}>
+      <Box component="div" className={styles.wrapper}>
         {type === 'JSON' && <StrNum content={content} />}
         <Box>
-          <Box component="div" className={css['input-wrapper']} data-replicated-value={content}>
+          <Box component="div" className={styles.inputWrapper} data-replicated-value={content}>
             <textarea value={content} onChange={handleChange} disabled={mode === 'view'}></textarea>
           </Box>
         </Box>
