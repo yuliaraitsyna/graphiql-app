b23ceb8d495246b81ffbc647aa6a0f5f2938d42a
Elizaveta <lizatukailo@gmail.com>
2024-09-04 22:19:39 +0300

    style: add margin to the url component

--- app/components/GraphiqlClient/GraphiqlClientRequest/Url/Url.tsx
+++ app/components/GraphiqlClient/GraphiqlClientRequest/Url/Url.tsx
@@ -1,9 +1,10 @@
 import {Grid, TextField, Typography} from '@mui/material';
 import {UrlProps} from './models';
+import styles from './Url.module.css';
 
 export const Url: React.FC<UrlProps> = ({label, name, value, onInput, onBlur, placeholder = ''}) => {
   return (
-    <Grid container spacing={2} alignItems="center">
+    <Grid className={styles.urlRow} container spacing={2} alignItems="center">
       <Grid item xs={12} sm={3}>
         <Typography component={'h6'} variant="h6">
           {label}:
