e339148fa0a2717e8b7a716a9d455516a0d086bb
Elizaveta <lizatukailo@gmail.com>
2024-09-09 21:58:30 +0300

    fix: change the regex string for checking url

--- app/utils/isValidUrl.ts
+++ app/utils/isValidUrl.ts
@@ -1,5 +1,5 @@
 export function isValidURL(urlString: string) {
   // eslint-disable-next-line no-useless-escape
-  const regex = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/;
+  const regex = /(?:https?):\/\/(\w+:?\w*)?(\S+)(:\d+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/;
   return regex.test(urlString);
 }
