5c2749d23bb9f62753b4e57f979e8ca356b9eb80
Elizaveta <lizatukailo@gmail.com>
2024-09-11 21:47:50 +0300

    feat: add route

---
+++ app/routes/graphql.$endpointUrlBase64encoded.tsx
@@ -0,0 +1,10 @@
+import {GraphiqlRequest} from '../components/GraphiqlClient/GraphiqlClientRequest';
+
+export default function GraphqlEndpointBodyRoute() {
+  return (
+    <>
+      {}
+      <GraphiqlRequest />;
+    </>
+  );
+}
