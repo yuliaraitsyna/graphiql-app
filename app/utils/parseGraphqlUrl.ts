d47d5c7c862b5d54b8a4d438a5118c8efa8fa041
Elizaveta <lizatukailo@gmail.com>
2024-09-12 19:07:25 +0300

    refactor: divide into smaller functions

--- app/utils/parseGraphqlUrl.ts
+++ app/utils/parseGraphqlUrl.ts
@@ -1,18 +1,35 @@
+import {Header} from '../components/HeadersEditor/models/header';
 import {extractVariablesFromGraphQLQuery} from './exctractVariablesFromQuery';
+
+function extractHeaderParams(searchParams: URLSearchParams): Header[] {
+  const headerParams: Header[] = [];
+  searchParams.forEach((value: string, key: string) => {
+    headerParams.push({
+      key: key,
+      value: value,
+      checked: true,
+    });
+  });
+  return headerParams;
+}
+
+function decodePathname(pathname: string) {
+  const parts = pathname.split('/');
+  const endpointBase64 = parts[2];
+  const bodyBase64 = parts[3];
+  const endpoint = atob(endpointBase64);
+  const queryUrl = atob(bodyBase64);
+  return {endpoint, queryUrl};
+}
+
 export function parseGraphQLUrl(url: string) {
   try {
     const urlObject = new URL(url);
-    const pathname = urlObject.pathname.split('/');
-    const endpointBase64 = pathname[2];
-    const bodyBase64 = pathname[3];
-    const endpoint = atob(endpointBase64);
-    const queryUrl = atob(bodyBase64);
+    const decodedPath = decodePathname(urlObject.pathname);
+    const {endpoint, queryUrl} = decodedPath;
     const {updatedQuery, variables} = extractVariablesFromGraphQLQuery(queryUrl);
     const query = updatedQuery ?? queryUrl;
-    const headerParams: Record<string, string> = {};
-    Object.entries(urlObject.searchParams).map(([value, key]) => {
-      headerParams[key] = value;
-    });
+    const headerParams = extractHeaderParams(urlObject.searchParams);
     return {
       endpoint,
       query,
