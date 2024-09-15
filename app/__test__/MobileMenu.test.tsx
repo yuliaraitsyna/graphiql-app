a685fd844414709dd708725e9f963da00ad9ad50
yuliaraitsyna <yuliaraitsyna@gmail.com>
2024-09-15 23:25:34 +0300

    fix: fix tests for mobile menu

--- app/__test__/MobileMenu.test.tsx
+++ app/__test__/MobileMenu.test.tsx
@@ -1,19 +1,45 @@
 import '@testing-library/jest-dom';
 import {render, screen, fireEvent} from '@testing-library/react';
 import {MobileMenu} from '../components/MobileMenu';
-import {useTranslation} from 'react-i18next';
-import {pages} from '../constants';
+import {AuthProvider} from '~/hooks/Authorization/AuthProvider';
 
 jest.mock('@remix-run/react', () => ({
   Link: jest.fn(),
 }));
+
 jest.mock('react-i18next', () => ({
   useTranslation: jest.fn(),
 }));
+
+jest.mock('@remix-run/react', () => ({
+  useNavigate: jest.fn(),
+  useLocation: jest.fn(),
+  useState: jest.fn(),
+}));
+
+jest.mock('react-i18next', () => ({
+  useTranslation: () => ({
+    t: (key: string) => {
+      const translations: Record<string, string | Record<number, {name: string}>> = {
+        'links.signIn': 'Sign in',
+        'links.signOut': 'Sign out',
+        'links.signUp': 'Sign up',
+        'links.mainPage': 'Main page',
+        'links.history': 'History',
+      };
+      return translations[key] || key;
+    },
+    i18n: {
+      changeLanguage: jest.fn(),
+    },
+  }),
+}));
+
 interface WhiteButtonProps {
   children: React.ReactNode;
   onClick: () => void;
 }
+
 jest.mock('../components/UI/WhiteButton', () => {
   const MockedWhiteButton: React.FC<WhiteButtonProps> = ({children, onClick}) => {
     return <button onClick={onClick}>{children}</button>;
@@ -27,41 +53,45 @@ describe('Test MobileMenu component', () => {
 
   beforeEach(() => {
     mockClose.mockClear();
-
-    (useTranslation as jest.Mock).mockReturnValue({
-      t: (key: string) => key,
-    });
   });
 
   test('Check if renders the menu when open', () => {
-    render(<MobileMenu anchorEl={document.createElement('div')} open={true} onClose={mockClose} />);
+    render(
+      <AuthProvider>
+        <MobileMenu anchorEl={document.createElement('div')} open={true} onClose={mockClose} />
+      </AuthProvider>,
+    );
 
-    expect(screen.getByText(pages.main.translationKey)).toBeInTheDocument();
-    expect(screen.getByText(pages.signOut.translationKey)).toBeInTheDocument();
-    expect(screen.getByText(pages.signIn.translationKey)).toBeInTheDocument();
-    expect(screen.getByText(pages.signUp.translationKey)).toBeInTheDocument();
+    expect(screen.getByText('Main page')).toBeInTheDocument();
+    expect(screen.getByText('Sign in')).toBeInTheDocument();
+    expect(screen.getByText('Sign up')).toBeInTheDocument();
   });
 
   test('Check if does not render the menu when not open', () => {
-    render(<MobileMenu anchorEl={null} open={false} onClose={mockClose} />);
+    render(
+      <AuthProvider>
+        <MobileMenu anchorEl={null} open={false} onClose={mockClose} />
+      </AuthProvider>,
+    );
 
-    expect(screen.queryByText(pages.main.translationKey)).not.toBeInTheDocument();
-    expect(screen.queryByText(pages.signOut.translationKey)).not.toBeInTheDocument();
+    expect(screen.queryByText('Main page')).not.toBeInTheDocument();
+    expect(screen.queryByText('Sign in')).not.toBeInTheDocument();
   });
 
   test('Check if calls onClose when menu items are clicked', () => {
-    render(<MobileMenu anchorEl={document.createElement('div')} open={true} onClose={mockClose} />);
+    render(
+      <AuthProvider>
+        <MobileMenu anchorEl={document.createElement('div')} open={true} onClose={mockClose} />
+      </AuthProvider>,
+    );
 
-    fireEvent.click(screen.getByText(pages.main.translationKey));
+    fireEvent.click(screen.getByText('Main page'));
     expect(mockClose).toHaveBeenCalled();
 
-    fireEvent.click(screen.getByText(pages.signOut.translationKey));
+    fireEvent.click(screen.getByText('Sign in'));
     expect(mockClose).toHaveBeenCalledTimes(2);
 
-    fireEvent.click(screen.getByText(pages.signIn.translationKey));
+    fireEvent.click(screen.getByText('Sign up'));
     expect(mockClose).toHaveBeenCalledTimes(3);
-
-    fireEvent.click(screen.getByText(pages.signUp.translationKey));
-    expect(mockClose).toHaveBeenCalledTimes(4);
   });
 });
